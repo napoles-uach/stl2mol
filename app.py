0a64730d98382082a639e5d919a9e730542dd9bd
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-10-21 20:16:04 -0600

    sub1

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def getC(cx,cy,cz):
 
 #smi=st.text_input("smile", exmp)
 st.markdown('# STL files from molecules')
-st.markdown('# Ready for 3D Printing or AR')
+st.markdown('## Ready for 3D Printing or AR')
 ids=st.sidebar.text_input("identifier, name, SMILES, etc", 'Caffeine')
 
 smi=CIRconvert(ids)
