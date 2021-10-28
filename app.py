c58fe3c3d9bcd4bf58ff540d2593ceae13bc473a
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-10-28 07:32:09 -0600

    no plotly

--- app.py
+++ app.py
@@ -98,66 +98,7 @@ combined = mesh.Mesh(numpy.concatenate(mesh_list))
 combined.save(filename, mode=stl.Mode.ASCII) 
 
 
-# Put your Python+Streamlit code here ...
-# you can modify it by double cliking on the folder icon at the left
-def stl2mesh3d(stl_mesh):
-    # stl_mesh is read by nympy-stl from a stl file; it is  an array of faces/triangles (i.e. three 3d points) 
-    # this function extracts the unique vertices and the lists I, J, K to define a Plotly mesh3d
-    p, q, r = stl_mesh.vectors.shape #(p, 3, 3)
-    # the array stl_mesh.vectors.reshape(p*q, r) can contain multiple copies of the same vertex;
-    # extract unique vertices from all mesh triangles
-    vertices, ixr = np.unique(stl_mesh.vectors.reshape(p*q, r), return_inverse=True, axis=0)
-    I = np.take(ixr, [3*k for k in range(p)])
-    J = np.take(ixr, [3*k+1 for k in range(p)])
-    K = np.take(ixr, [3*k+2 for k in range(p)])
-    return vertices, I, J, K
-#my_mesh = mesh.Mesh.from_file('combined.stl')
-my_mesh = mesh.Mesh.from_file(filename)
-vertices, I, J, K = stl2mesh3d(my_mesh)
-x, y, z = vertices.T
 
-colorscale= [[0, '#e5dee5'], [1, '#e5dee5']]  
-
-mesh3D = go.Mesh3d(
-            x=x,
-            y=y,
-            z=z, 
-            i=I, 
-            j=J, 
-            k=K, 
-            flatshading=True,
-            colorscale=colorscale, 
-            intensity=z, 
-            name='AT&T',
-            showscale=False)
-
-
-title = ""
-layout = go.Layout(paper_bgcolor='rgb(0.999,0.999,0.999)',
-            title_text=title, title_x=0.5,
-                   font_color='brown',
-            width=800,
-            height=800,
-            scene_camera=dict(eye=dict(x=1.25, y=-1.25, z=1)),
-            scene_xaxis_visible=False,
-            scene_yaxis_visible=False,
-            scene_zaxis_visible=False)
-
-fig = go.Figure(data=[mesh3D], layout=layout)
-fig.data[0].update(lighting=dict(ambient= 0.18,
-                                 diffuse= 1,
-                                 fresnel=  .1,
-                                 specular= 1,
-                                 roughness= .1,
-                                 facenormalsepsilon=0))
-fig.data[0].update(lightposition=dict(x=3000,
-                                      y=3000,
-                                      z=10000));
-
-fig.show()
-raw_html=fig.to_html()
-#raw_html = base64.b64encode(raw_html).decode()
-components.html(raw_html,height=900)
 
 with open(filename, 'rb') as my_file:
     st.sidebar.download_button(label = 'Download .stl file', data = my_file, file_name = filename, )   
